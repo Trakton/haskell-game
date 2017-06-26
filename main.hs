bd0feb696ba4a181b648473ada307f6f9097ca7e
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2017-06-26 15:57:01 -0300

    [pong] basic bar, ball and map implemented

--- main.hs
+++ main.hs
@@ -1,10 +1,53 @@
 module Main where
-
 import Graphics.UI.Fungen
 
-main :: IO ()
-main =
-  let winConfig = ((50,50),(500,300),"Hello, Fungen World! Press Q to quit")
-      gameMap = colorMap 0.0 0.0 0.0 250 250
-      bindings = [(Char 'q', Press, \_ _ -> funExit)]
-  in funInit winConfig gameMap [] () () bindings (return()) Idle []
+data GameAttribute = Score Int
+
+type PongObject = GameObject ()
+type PongAction a = IOGame GameAttribute () () () a
+
+createBall :: PongObject
+createBall = let ballPic = Basic(Circle 3.0 0.0 1.0 0.0 Filled)
+             in object "ball" ballPic False (125,125) (-5,5) ()
+
+createBar :: PongObject
+createBar = let barBound = [(-25, -6), (25,-6), (25,6), (-25,6)]
+                barPic = Basic(Polyg barBound 1.0 1.0 1.0 Unfilled)
+            in object "bar" barPic False (125,30) (0,0) ()
+
+moveBarToRight :: Modifiers -> Position -> PongAction ()
+moveBarToRight _ _ = do
+  obj <- findObject "bar" "barGroup"
+  (pX,pY) <- getObjectPosition obj
+  (sX,_) <- getObjectSize obj
+  if(pX + (sX/2) + 5 <= 250) then
+    setObjectPosition (pX+5,pY) obj
+  else
+    setObjectPosition ((250-(sX/2)), pY) obj
+
+moveBarToLeft :: Modifiers -> Position -> PongAction ()
+moveBarToLeft _ _ = do
+  obj <- findObject "bar" "barGroup"
+  (pX,pY) <- getObjectPosition obj
+  (sX,_) <- getObjectSize obj
+  if(pX +(sX/2) - 5 >= 0) then
+    setObjectPosition (pX-5, pY) obj
+  else
+    setObjectPosition (sX/2, pY) obj
+
+gameCycle :: PongAction ()
+gameCycle = do
+  (Score n) <- getGameAttribute
+  printOnScreen (show n) TimesRoman24 (0,0) 1.0 1.0 1.0
+
+main :: IO()
+main = do
+  let winConfig =((0,0),(250,250), "Test")
+      bmpList = [("tex.bmp", Nothing)]
+      gameMap = textureMap 0 50 50 250.0 250.0
+      bar = objectGroup "barGroup" [createBar]
+      ball = objectGroup "ballGroup" [createBall]
+      initScore = Score 0
+      input = [(SpecialKey KeyRight, StillDown, moveBarToRight),
+               (SpecialKey KeyLeft, StillDown, moveBarToLeft)]
+  funInit winConfig gameMap [bar,ball] () initScore input gameCycle (Timer 40) bmpList
